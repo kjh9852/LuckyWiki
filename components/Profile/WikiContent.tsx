22c56c711f657f68cf917e946f9ad8f8818aa888
marie1016 <kjo5281@gmail.com>
2024-09-01 18:32:10 +0900

    #5 design: EmptyContent

--- components/Profile/WikiContent.tsx
+++ components/Profile/WikiContent.tsx
@@ -5,6 +5,20 @@ interface WikiContentProps {
   profile: ProfileType;
 }
 
+function EmptyContent() {
+  return (
+    <div className={styles.emptyContent}>
+      <p>
+        아직 작성한 내용이 없네요.<br>위키에 참여해 보세요!</br>
+      </p>
+      <button>시작하기</button>
+    </div>
+  );
+}
+
 export default function WikiContent({ profile }: WikiContentProps) {
+  if (!profile.content) {
+    return <EmptyContent />;
+  }
   return <div className={styles.wikiContent}>{profile.content}</div>;
 }
