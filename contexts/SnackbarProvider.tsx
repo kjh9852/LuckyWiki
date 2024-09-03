d8ec1240cf0c18675d407adb120e055d8d4738db
Minchan <98478661+itscold96@users.noreply.github.com>
2024-09-03 17:03:27 +0900

    #14 feat: 공용 스낵바 호출 커스텀 훅 제작

---
+++ contexts/SnackbarProvider.tsx
@@ -0,0 +1,53 @@
+import { createContext, ReactNode, useContext } from 'react';
+import { message, Space } from 'antd';
+import iconSuccess from '@/public/icon/icon-success.png';
+import iconError from '@/public/icon/icon-error.png';
+import Image from 'next/image';
+
+type SnackBarType = 'success' | 'error';
+
+interface SnackBarParams {
+  type: SnackBarType;
+  content: string;
+}
+
+interface SnackBarContextValue {
+  openSnackBar: ({ type, content }: SnackBarParams) => void;
+}
+
+const SnackbarContext = createContext<SnackBarContextValue | undefined>(undefined);
+
+export default function SnackBarProvider({ children }: { children: ReactNode }) {
+  const [messageApi, contextHolder] = message.useMessage({ top: 120 });
+
+  const iconSrc: Record<SnackBarType, ReactNode> = {
+    success: <Image src={iconSuccess} alt={'성공 메시지 아이콘'} height={20} width={20} />,
+    error: <Image src={iconError} alt={'실패 메시지 아이콘'} height={20} width={20} />,
+  };
+
+  const openSnackBar = ({ type, content }: SnackBarParams) => {
+    return messageApi.open({
+      type,
+      content,
+      className: `custom-snackbar custom-snackbar-${type}`,
+      icon: iconSrc[type],
+    });
+  };
+
+  return (
+    <SnackbarContext.Provider value={{ openSnackBar }}>
+      {contextHolder}
+      {children}
+    </SnackbarContext.Provider>
+  );
+}
+
+export const useSnackBar = () => {
+  const context = useContext(SnackbarContext);
+
+  if (!context) {
+    throw new Error('반드시 SnackBarProvider 안에서 사용해야 합니다.');
+  }
+
+  return context;
+};
