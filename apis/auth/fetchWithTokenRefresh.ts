fd2a48ccd94cf69ca461f8e43338879c68cd7d24
Minchan <98478661+itscold96@users.noreply.github.com>
2024-09-06 19:03:04 +0900

    #3 fix: 엑세스토큰을 외부에서 받지 않아도 되도록 fetchWithTokenRefresh 수정

--- apis/auth/fetchWithTokenRefresh.ts
+++ apis/auth/fetchWithTokenRefresh.ts
@@ -1,36 +1,47 @@
 import { getTokens } from '@/utils/getTokens';
 import { getNewAccessToken } from './getNewAccessToken';
+import { CookieValueTypes, setCookie } from 'cookies-next';
 
-interface fetchWithTokenRefreshParams {
-  fetchUrl: string;
-  options: {
-    method: 'GET' | 'POST' | 'PATCH';
-    headers: Record<string, string>;
-    body: string;
-  };
+interface FetchOptions {
+  method: 'GET' | 'POST' | 'PATCH';
+  headers: Record<string, string>;
+  body?: string;
 }
 
-export const fetchWithTokenRefresh = async ({ fetchUrl, options }: fetchWithTokenRefreshParams) => {
-  const response = await fetch(fetchUrl, options);
+const insertAuthorization = (options: FetchOptions, accessToken: string | CookieValueTypes) => {
+  // headers에 accessToken 주입
+  options.headers = {
+    ...options.headers,
+    Authorization: `Bearer ${accessToken}`,
+  };
+
+  return options;
+};
+
+export const fetchWithTokenRefresh = async (fetchUrl: string, options: FetchOptions) => {
+  const { refreshToken, accessToken } = getTokens();
+  const authorizationOptions = insertAuthorization(options, accessToken);
+
+  // accessToken 넣은 options로 fetch 요청
+  const response = await fetch(fetchUrl, authorizationOptions);
 
   if (response.status === 401) {
     // accessToken이 만료된 경우인 401 에러라면 refreshToken으로 accessToken 재발급
-    const { refreshToken } = getTokens();
     const newAccessToken = await getNewAccessToken(refreshToken);
 
     if (!newAccessToken) {
-      // refreshToken 도 만료되었다면 undefined 반환
+      // refreshToken도 만료되었다면 undefined 반환
       return undefined;
     }
 
-    // 새 토큰으로 헤더 업데이트
-    options.headers = {
-      ...options.headers,
-      Authorization: `Bearer ${newAccessToken}`,
-    };
+    // 쿠키 업데이트
+    setCookie('accessToken', newAccessToken);
+
+    // 새 accessToken을 주입한 options
+    const newOptions = insertAuthorization(options, newAccessToken);
 
     // 재발급한 accessToken으로 다시 유저 정보 요청
-    const newResponse = await fetch(fetchUrl, options);
+    const newResponse = await fetch(fetchUrl, newOptions);
     return await newResponse.json();
   }
 
