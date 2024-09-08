3ea790127653dca6777883d5fe95a81ffa072728
Minchan <98478661+itscold96@users.noreply.github.com>
2024-09-08 20:33:22 +0900

    #26 feat: 메인 페이지 애니메이션 효과 부여

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -3,6 +3,7 @@ import ProfileType from '@/types/types';
 import styles from './Home.module.scss';
 import classNames from 'classnames';
 import { GetServerSideProps } from 'next';
+import { Bounce, Fade, JackInTheBox } from 'react-awesome-reveal';
 
 interface HomeProps {
   profileList: ProfileType[];
@@ -33,18 +34,24 @@ export default function Home({ profileList }: HomeProps) {
   return (
     <div className={styles.homeContainer}>
       <section className={styles.paragraph}>
-        <h1 className={styles.textXl}>
-          친구들이
-          <br />
-          내 위키를 만들어준다니,
-          <br />
-          완전 <span className={classNames(styles.textXl, styles.cloverText, styles.green)}>럭키위키</span>자나!?
-        </h1>
-        <p className={classNames(styles.description, styles.textMd)}>
-          친구들이 만들어 준 나만의 <span className={classNames(styles.cloverText, styles.green)}>위키</span>
-          <br />
-          친구들의 위키도 구경해보세요<span className={styles.cloverText}>!</span>
-        </p>
+        <Fade>
+          <h1 className={styles.textXl}>
+            친구들이
+            <br />내 위키를 만들어준다니,
+          </h1>
+        </Fade>
+        <Bounce delay={500}>
+          <h1 className={styles.textXl}>
+            완전 <span className={classNames(styles.textXl, styles.cloverText, styles.green)}>럭키위키</span>자나!?
+          </h1>
+        </Bounce>
+        <JackInTheBox delay={900}>
+          <p className={classNames(styles.description, styles.textMd)}>
+            친구들이 만들어 준 나만의 <span className={classNames(styles.cloverText, styles.green)}>위키</span>
+            <br />
+            친구들의 위키도 구경해보세요<span className={styles.cloverText}>!</span>
+          </p>
+        </JackInTheBox>
       </section>
       <section className={styles.cardListContainer}>
         {profileList?.map((profile, index) => <ProfileCard key={profile.id} profile={profile} index={index} />)}
