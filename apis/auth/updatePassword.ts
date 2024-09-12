e11d8bef9a05d582739f2ce1609875c12bb32d0b
Minchan <98478661+itscold96@users.noreply.github.com>
2024-09-12 18:35:17 +0900

    #40 fix: updatePassword()에서 삭제된 FormInputValues 타입을 import 함

--- apis/auth/updatePassword.ts
+++ apis/auth/updatePassword.ts
@@ -1,10 +1,10 @@
-import { FormInputValues } from '@/hooks/useValidForm';
 import { fetchWithTokenRefresh } from './fetchWithTokenRefresh';
 
-type updatePasswordParams = Record<
-  keyof Pick<FormInputValues, 'currentPassword' | 'password' | 'passwordConfirmation'>,
-  string
->;
+type updatePasswordParams = {
+  currentPassword: string;
+  password: string;
+  passwordConfirmation: string;
+};
 
 export const updatePassword = async ({ currentPassword, password, passwordConfirmation }: updatePasswordParams) => {
   const response = await fetchWithTokenRefresh(`${process.env.NEXT_PUBLIC_BASE_URL}/users/me/password`, {
