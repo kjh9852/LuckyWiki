62f9467cd430ba1963a2759cf52d65f751ecf9c4
kjh9852 <kjh985264@gmail.com>
2024-09-13 23:28:50 +0900

    #55 fix:대소문자 구분 안 된 폴더 합치기

--- components/WikiList/SPinner.tsx
+++
@@ -1,6 +0,0 @@
-import Image from 'next/image';
-import styles from './Spinner.module.scss';
-
-export default function Spinner() {
-  return <Image className={styles.spinner} src={'/icon/icon-spinner.png'} width={45} height={45} alt="로딩중" />;
-}
