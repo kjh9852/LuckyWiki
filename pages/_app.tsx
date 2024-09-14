cf59671d8df61aae9fb7e17c67ba3623c4dce43d
Minchan <98478661+itscold96@users.noreply.github.com>
2024-09-14 16:40:44 +0900

    #54 fix: 실제 모바일 환경을 고려한 이미지 비율 조정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,19 @@ import Providers from '@/contexts/Providers';
 import '@/styles/globals.scss';
 import '@/styles/quillCustom.scss';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Providers>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </Providers>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Providers>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </Providers>
+    </>
   );
 }
