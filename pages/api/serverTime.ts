4e9b39f85067b479a0ce177989a9e5b60a1ff168
kjh9852 <kjh985264@gmail.com>
2024-09-16 23:26:31 +0900

    feat: 타입선언

--- pages/api/serverTime.ts
+++ pages/api/serverTime.ts
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const serverTime = Date.now(); // 현재 서버 시간 (밀리초)
   res.status(200).json({ serverTime });
 }
