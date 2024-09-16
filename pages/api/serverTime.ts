94477089a24d6f81c3bfc48e47f4982869d7b4a6
kjh9852 <kjh985264@gmail.com>
2024-09-17 00:55:33 +0900

    reafctor: dayjs로 현재시간 가져오기

--- pages/api/serverTime.ts
+++ pages/api/serverTime.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import dayjs from 'dayjs';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const serverTime = Date.now(); // 현재 서버 시간 (밀리초)
+  const serverTime = dayjs().valueOf(); // 현재 서버 시간 (밀리초)
   res.status(200).json({ serverTime });
 }
