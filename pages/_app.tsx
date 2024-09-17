9c1302c274ec414d2e8ca70334f445de22502217
kjh9852 <kjh985264@gmail.com>
2024-09-18 03:23:29 +0900

    chore: 오픈그래프,파비콘 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,13 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>럭키위키</title>
+        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="친구들이 만드는 나만의 위키" />
+        <meta property="og:title" content="럭키위키" />
+        <meta property="og:description" content="친구들이 만드는 나만의 위키" />
+        <meta property="og:image" content="/og_image.png" />
       </Head>
       <Providers>
         <Header />
